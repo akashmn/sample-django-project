1eecf23f43a262a78037c1f508b8612338de9554
akashmn <akash003nandan@gmail.com>
2024-02-21 20:43:36 +0530

    using tags (if-else & for-loops)

--- sample_project/hello_app/views.py
+++ sample_project/hello_app/views.py
@@ -3,12 +3,40 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def print_hello(request):
-    movie_details = {
+    movie_data = { 
+        'movies' :[ 
+    {
+        "title" : "The Godfather",
+        "year" : "1972",
+        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    },
+
+    {
+        "title" : "The Godfather",
+        "year" : "1972",
+        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    },
+
+    {
+        "title" : "The Godfather",
+        "year" : "1972",
+        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    },
+
+    {
+        "title" : "The Godfather",
+        "year" : "1972",
+        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    },
+
+    {
         "title" : "The Godfather",
         "year" : "1972",
         "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
     }
-    return render(request, 'hello.html', movie_details)
+    
+    ]}
+    return render(request, 'hello.html', movie_data)
     
 
 # Create your views here.
