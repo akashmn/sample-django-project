7b1bce7e3cccf692baff0c2c678ddcb2747b6c9d
akashmn <akash003nandan@gmail.com>
2024-02-21 19:13:16 +0530

    templates and render function

--- sample_project/hello_app/views.py
+++ sample_project/hello_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def print_hello(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'hello.html', {})
     
 
 # Create your views here.
--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sample_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
