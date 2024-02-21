f2febabd4e793335b4440f1d82156b4b48b90472
akashmn <akash003nandan@gmail.com>
2024-02-21 20:27:59 +0530

    passing server data to views

--- sample_project/hello_app/views.py
+++ sample_project/hello_app/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def print_hello(request):
-    return render(request, 'hello.html', {})
+    movie_details = {
+        "title" : "The Godfather",
+        "year" : "1972",
+        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    }
+    return render(request, 'hello.html', movie_details)
     
 
 # Create your views here.
