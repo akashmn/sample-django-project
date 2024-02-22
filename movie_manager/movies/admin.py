cfe15ef8e2f74fe9a4ed464a157e506d2687d5fa
akashmn <akash003nandan@gmail.com>
2024-02-22 18:27:40 +0530

    django-admin (register)

--- movie_manager/movies/admin.py
+++ movie_manager/movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Movie_info, Director
+
+#To see the tables in the admin panel, we need to register the models here.
+
+admin.site.register(Movie_info)
+admin.site.register(Director)
+
 # Register your models here.
