51b41aecc174d57c6c521490dd4f75d6c7bef0c5
akashmn <akash003nandan@gmail.com>
2024-02-22 13:18:42 +0530

    ORM, setting up feilds in models.py  & making migrations

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,3 +1,12 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Movie_info(models.Model):
+    title =  models.CharField(max_length=100)
+    year = models.IntegerField(null = True)
+    description = models.TextField()
+
+#Object-relational mapping (ORM) is a way to align programming code with database structures. ORM uses metadata descriptors to create a layer between the programming language and a relational database.
+
+
