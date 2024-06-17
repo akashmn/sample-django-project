6484f43dec30aeac30932d4a9fdc7a9f3bb6e40e
Akash M Nandan <akash003nandan@gmail.com>
2024-06-17 13:20:18 +0530

    clean branch

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -12,5 +12,7 @@ class Director(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField(null = True)
 
+
+
 #Object-relational mapping (ORM) is a way to align programming code with database structures. ORM uses metadata descriptors to create a layer between the programming language and a relational database.
 
